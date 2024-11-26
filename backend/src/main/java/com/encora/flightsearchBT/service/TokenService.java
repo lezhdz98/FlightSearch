3512e0f7f1a6ae8aea175886177aaafac873dcd0
lezhdz98 <Francisco.lezama@encora.com>
2024-11-25 18:13:21 -0600

    Update TokenService.java for ssl certificates

--- backend/src/main/java/com/encora/flightsearchBT/service/TokenService.java
+++ backend/src/main/java/com/encora/flightsearchBT/service/TokenService.java
@@ -11,6 +11,16 @@ import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.X509Certificate;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+
 @Service
 public class TokenService {
 
@@ -22,11 +32,33 @@ public class TokenService {
     public TokenService(@Value("${amadeus.client_id}") String clientId,
                         @Value("${amadeus.client_secret}") String clientSecret,
                         @Value("${amadeus.url}") String urlAmadeus,
-                        RestTemplate restTemplate) {
+                        RestTemplateBuilder restTemplateBuilder) throws NoSuchAlgorithmException, KeyManagementException {
         this.clientId = clientId;
         this.clientSecret = clientSecret;
         this.urlAmadeus = urlAmadeus;
-        this.restTemplate = restTemplate;
+        this.restTemplate = getRestTemplate(restTemplateBuilder);
+    }
+
+    private RestTemplate getRestTemplate(RestTemplateBuilder restTemplateBuilder) throws NoSuchAlgorithmException, KeyManagementException {
+        SSLContext sslContext = SSLContext.getInstance("TLS");
+        TrustManager[] trustManagers = new TrustManager[]{new X509TrustManager() {
+            @Override
+            public void checkClientTrusted(X509Certificate[] x509Certificates, String s) {
+            }
+
+            @Override
+            public void checkServerTrusted(X509Certificate[] x509Certificates, String s) {
+            }
+
+            @Override
+            public X509Certificate[] getAcceptedIssuers() {
+                return new X509Certificate[0];
+            }
+        }};
+        sslContext.init(null, trustManagers, null);
+        HttpsURLConnection.setDefaultSSLSocketFactory(sslContext.getSocketFactory());
+        HttpsURLConnection.setDefaultHostnameVerifier((hostname, session) -> true);
+        return restTemplateBuilder.requestFactory(SimpleClientHttpRequestFactory.class).build();
     }
 
     public AmadeusAuthorization requestToken() {
@@ -44,4 +76,7 @@ public class TokenService {
 
         return response.getBody();
     }
+
+
 }
+
